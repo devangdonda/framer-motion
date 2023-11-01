d7bd050929be556a9d3fd1d7268123de5ee2a654
devangdonda <dkdonda@mun.ca>
2023-10-31 22:18:53 -0230

    second

--- src/ComponentA.js
+++ src/ComponentA.js
@@ -2,29 +2,63 @@ import React from "react";
 import "./ComponentA.css";
 import { motion } from "framer-motion";
 
-const firstVariants = {
-  animation: {
+const parentVariants = {
+  visible: {
     x: 0,
-    type: "spring",
     transition: {
-      delay: 0.5,
-      stiffness: 120,
+      type: "spring",
+      mass: 1,
+      damping: 10,
+      when: "beforeChildren",
+      staggerChildren: 2,
     },
   },
-  init: {
+  hidden: {
     x: "-100vw",
   },
 };
+
+const childVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
+const buttonVariants = {
+    hover: {
+        scale: 1.2,
+        transition: {
+            duration: 0.3,
+            repeatType: 'mirror',
+            repeat: Infinity
+        }
+    }
+}
 const ComponentA = () => {
   return (
-    <motion.div
-      className="main-box"
-      variants={firstVariants}
-      initial="init"
-      animate="animation"
-    >
-      Hello Bro
-    </motion.div>
+    <>
+      <motion.div
+        className="main-box"
+        variants={parentVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        Hello Bro
+        <motion.div className="child1-box" variants={childVariants}>
+          Hello Bro
+        </motion.div>
+        <motion.div className="child2-box" variants={childVariants}>
+          Hello Bro
+        </motion.div>
+      </motion.div>
+      <motion.button className="button"
+      variants={buttonVariants}
+      whileHover='hover'>
+        Click Me
+      </motion.button>
+    </>
   );
 };
 
